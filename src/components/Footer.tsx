60fbb9cffff50a9697497f39157b17ef7f273e50
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-10-09 19:51:50 -0300

    fix: update footer date format to include day and month

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,15 @@ import "./Layout.css";
 import { Link } from "react-router";
 
 export default function Footer() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const currentDay = String(currentDate.getDate()).padStart(2, "0");
+
   return (
     <footer className="site-footer">
       <p>
-        © {new Date().getFullYear()} Community Articles —{" "}
+        © {currentDay}.{currentMonth}.{currentYear} Community Articles —{" "}
         <Link
           to="https://github.com/gabriel-logan/community-articles"
           target="_blank"
