1774c32669b0f172fea3d9aab66a104ab8937010
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-10-09 21:10:14 -0300

    feat: add route for AboutPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import AboutPage from "./pages/About";
 import ArticlesPage from "./pages/ArticlesPage";
 import HomePage from "./pages/Home";
 import NotFoundPage from "./pages/NotFound";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/articles/:author/:slug" element={<ArticlesPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
